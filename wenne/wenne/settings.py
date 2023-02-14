ac675fb5555e5aa6377bfdcf991976450bfb5f0a
Rayanecarlos <rayanemoura46@gmail.com>
2023-02-13 22:09:22 -0300

    primeiro commit

--- wenne/wenne/settings.py
+++ wenne/wenne/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,9 +122,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-STAT
 
+STATICFILES_DIRS = [
+    'media',
+    'static',
 
-MIDIA_URL='/midia/'
+]
+
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
